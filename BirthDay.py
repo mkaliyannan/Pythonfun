09ce16114e3120f6e82c82300c0f9b00f8fbed11
mk@logictech.ca <Mohan.Kaliyannan@maplenetworks.ab.ca>
2021-07-16 15:12:44 -0600

    Modified need to fix the bug

--- BirthDay.py
+++ BirthDay.py
@@ -1,6 +1,7 @@
 import datetime as t
+
 curDate = t.datetime.now()
-birDate = t.datetime(1985, 7, 16)
+birDate = t.datetime(1985, 7, 17)
 curDay = curDate.day
 curMon = curDate.month
 curYear = curDate.year
@@ -11,41 +12,57 @@ birYear = birDate.year
 months = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 
 if birDay > curDay:
-    curDay = curDay+months[curMon]-birDay
-    curMon = curMon-1
+    curDay = curDay + months[curMon] - birDay
+    curMon = curMon - 1
 
 if birMon > curMon:
-    curMon = curMon+12-birMon
-    curYear = curYear-1-birYear
+    curMon = curMon + 12 - birMon
+    curYear = curYear - 1 - birYear
 
-if birMon == curMon and birDate > curDate:
-    curDay = curDay+months[curMon]-birDay
-    curMon = curMon-1
 
-if birMon == curMon and birDate < curDate:
-    curMon = curMon-birMon
-    curYear = curYear-birYear
+if birMon == curMon and birDay > curDay:
+    curDay = curDay + months[curMon] - birDay
+    curMon = curMon - 1
 
-if birMon == curMon and birDate == curDate:
-    curYear = curYear-birYear
-    curMon  = curMon-birMon
-    curDate = curDate-birDate
-    print("Happy birthday. You are {} Yrs old.".format(curYear))
+if birMon == curMon and birDay < curDay:
+    curMon = curMon - birMon
+    curYear = curYear - birYear
+    curDay = curDay - birDay
 
-print("\n\nYour age is :\n\n{} Years {} Months and {} Days old.\n\n".format(curYear, curMon, curDay))
+if birDay == curDay and birMon == curMon:
+    curYear = curYear - birYear
+    curMon = curMon - birMon
+    curDay = curDay - birDay
+
+print("\n\nYour age is :\n\n{} Years {} Months and {} Days old.\n\n".format(
+    curYear, curMon, curDay))
+
+if curMon == curDay:
+    print("\n\nHappy Birthday !\n\n\n")
+'''
+if birMon == curDate.month and birDay < curDate.day:
+        nbd = t.datetime(nxtYear, birMon, birDay).date()
+        cdy = t.datetime.now().date()
+        x = nbd-cdy
+        print("Your birthday will be in next {} days".format(x.days)) 
 
 if birMon == curDate.month and birDay > curDate.day:
         nbd = t.datetime(nxtYear, birMon, birDay).date()
         cdy = t.datetime.now().date()
         x = nbd-cdy
-        print("Your birthday will be in next {} days".format(x.days))
+        print("Your birthday will be in next {} days".format(x.days)) 
+
+
 if birMon > curDate.month:
         nbd = t.datetime(nxtYear, birMon, birDay).date()
         cdy = t.datetime.now().date()
         x = nbd-cdy
-        print("Your birthday will be in next {} days".format(x.days))       
+        print("Your birthday will be in next {} days".format(x.days))  
+          
+
 if birMon < curDate.month:
         nbd = t.datetime(nxtYear+1, birMon, birDay).date()
         cdy = t.datetime.now().date()
         x = nbd-cdy
         print("Your birthday will be in next {} days".format(x.days))
+'''
